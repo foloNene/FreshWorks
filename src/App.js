c735bbd7663acc5209564228baf7cc3cb08b31a5
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-02-23 15:01:27 -0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./App.css"
+
+
+function App(){
+
+  return(
+    <div className='Container'>
+      <div className='Header'>
+        <div className='HeaderRapper'>
+        <div> Logo </div>
+        <div className='Nav'>
+          <p>Product</p>
+          <p>Product</p>
+          <p>Product</p>
+          <p>Product</p>
+          <p>Product</p>
+          <p>Product</p>
+        </div>
+        </div>
+
+      </div>
+      <div className='HeroHolder'>
+        HERO
+      </div>
+    </div>
+  );
+}
+
+export default App;
